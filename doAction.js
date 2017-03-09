0926f0054b4d7c664394e9d9c418007ae72a5408
Igor Tkachenko <vash.igor@gmail.com>
2017-03-09 16:31:55 +0300

    Update doAction.js

--- doAction.js
+++ doAction.js
@@ -36,9 +36,18 @@ span2 = document.getElementById('span2');
 
 window.doAction = function () {
 
+    // You shouldn't change values here - more over it doesn't make any sense
+    // clickCounter is a function without properties
     clickNumber += clickCounter.arguments[0];
     //count = clickCounter.arguments[1];
+    // It still doesn't make sense because you pass the countx result not the callback itself
+    // Call shoul look like clickCounter(clickNumber, clickNumber % 2 === 0 ? count1 : count2, count) 
+    // We pass the clickNumber as a value that has to be incrmeneted, we pass the function as a function, 
+    // and the value we are working with
     clickCounter(clickNumber % 2 == 0 ? count1(count) : count2(count));
+    // Click counter returns new value os count and clickNumber as an array. You should write it into some variable
+    // var results = ...
+    // Then the values should be got out of it: e.g. count = result[1]
     console.log(count, clickNumber);
     span.innerHTML = count;
     span2.innerHTML = clickNumber;
